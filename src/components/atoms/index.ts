38ea13e40e76670035d7b93d2081dd5b1ae25523
Patrik Dvořáček <patrik@dvoracek.dev>
2023-02-12 14:13:14 +0100

    refactor(components): 🎨 Update the indexes of components to a more reasonable structure.

---
+++ src/components/atoms/index.ts
@@ -0,0 +1,4 @@
+export * from './base/Base';
+export * from './profilePicture/ProfilePicture';
+export * from './social/Social';
+export * from './title/Title';
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,2 @@
-export * from './atoms/base/Base';
-export * from './atoms/profilePicture/ProfilePicture';
-export * from './atoms/social/Social';
-export * from './atoms/title/Title';
-
-export * from './molecules/header/Header';
+export * from './atoms';
+export * from './molecules';
